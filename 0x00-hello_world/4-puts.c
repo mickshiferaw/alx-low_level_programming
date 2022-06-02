4d7bf1498641c9fc109ec31a379dcfe1f04302ef
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-02 15:50:12 -0700

    4-puts.c 9

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/** 
- * main -entry point
- * return: returns zero
+/**
+ * main - Entry point of the code
+ * Return: Always 0
  *
-**/
+ */
 int main(void)
 {
 	char *s = "\"Programming is like building a multilingual puzzle";
