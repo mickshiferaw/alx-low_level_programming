fb8b20cd97bd025846ce404dcd07c492e21b8ce8
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-02 15:15:53 -0700

    4-puts.c 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* uses puts function */
 int main(void)
 {
-	char *s = "Programming is like building a multilingual puzzle\n";
+	char *s = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(s);
 	return (0);
