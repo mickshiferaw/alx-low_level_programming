6198db6b6ae7f4b93927dd1bdd5cd16b6931b349
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-03 15:43:32 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Main code will display hexadecimal
+ *
+ * Return: Returns zero
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
