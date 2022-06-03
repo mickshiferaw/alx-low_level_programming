bf069d86c64e1f60fad061a0bf6493f3dc6f0eac
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-03 15:15:04 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - The main code prints singe digit numbers using putchar
+ *
+ * Return: Returns zero
+ */
+int main(void)
+{
+	int m;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
+
