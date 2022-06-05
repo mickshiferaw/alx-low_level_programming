de3716ab5737458442010ed40a3a2a1d63607df4
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-05 15:02:13 -0700

    9-print_comb.c b

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Main code prints single digits
+ * Return: Returns zero
+ */
+int main(void)
+{
+	int m;
+
+	for (m = '0'; m <= '9'; m++)
+	{putchar(m);
+	putchar (',');
+	putchar (' ');
+	}
+	return (0);
+}
