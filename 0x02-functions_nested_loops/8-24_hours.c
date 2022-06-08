2c5107af706d0f8b6d7275239ff8ee731e2a24ae
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-08 14:17:27 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * jack_bauer - this function prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0 m < 60; m++)
+		{
+		if (h < 10)
+		{
+		_putchar('0');
+		_putchar(h + '0');
+		}
+			else if (h >= 10)
+			{
+				_putchar((h / 10) + '0');
+				_putchar((h % 10) + '0');
+			}
+			if (m < 10)
+			{
+			_putchar(':');
+			_putchar('0');
+			_putchar(m + '0');
+			}
+			else if (m >= 10)
+			{
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			}
+			_putchar('\n');
+		}
+
+	}
+}
