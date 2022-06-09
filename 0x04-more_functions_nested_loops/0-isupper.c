5fcd6da9222e00f8b1df777f087ab63b3148d71b
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-09 08:44:39 -0700

    0-isupper.c 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _isupper - This function checks if letter is uppercase or not
+ * @c: argument
  * Return: Returns 0 or 1 depending on the function
  */
 int _isupper(int c)
