94c617e111d696533152d409276f60f626be3e37
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-09 14:18:06 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - Prints 0 t0 14 10 times
+ */
+void more_numbers(void)
+{
+	int m, b;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			_putchar(b + '0');
+		}
+		_putchar('\n');
+	}
+}
+
