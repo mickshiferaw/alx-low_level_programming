012564765368d0b40a91b0c1c771b3f055d0cebf
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-09 08:36:40 -0700

    int _isupper(int c)

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _isupper - This function checks if letter is uppercase or not
+ * Return: Returns 0 or 1 depending on the function
+ */
+int _isupper(int c)
+{
+	return (1);
+}
