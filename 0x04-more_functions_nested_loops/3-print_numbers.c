2fc4318b5033ea83e739ea3fcc4af764de96d537
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-09 12:48:55 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Prints number from 0 to 9
+ */
+void print_numbers(void)
+{
+	int m;
+
+	for (m >= 0; m <= 9; m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
