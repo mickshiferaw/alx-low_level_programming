47780c02341f4f3bcd8a0bb1296d359d7578e47a
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-09 13:55:45 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints number except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int m;
+
+	for (m = 0; m <= 9; m++)
+	{
+		if (m != 2 && m != 4)
+		_putchar(m + '0');
+	}
+	_putchar('\n');
