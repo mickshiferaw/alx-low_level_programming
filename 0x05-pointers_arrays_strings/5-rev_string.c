cbd7fd9884c7c5201ae2764528256a60666a153a
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-13 15:38:56 -0700

    5 fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,15 +5,22 @@
  */
 void rev_string(char *s)
 {
-	int a, b;
+	char tmp;
+	int a, b, c;
 
 	b = 0;
+	c = 0;
 
 	while (s[b] != '\0')
-	b++
+	b++;
 
-	for (a = b - 1; a >= 0; a--)
+	c = b - 1
+	for (a = 0; a < b; a++)
 	{
-		_putchar(s[a]);
+	tmp = s[a];
+	s[a] = s[c];
+	s[c] = tmp;
+	c -= 1;
 	}
+
 }
