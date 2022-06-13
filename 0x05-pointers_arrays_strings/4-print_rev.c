992f1fa8d4b26f6e9fd36bfe8fcdd18da31ec4f2
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-13 14:42:35 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - this functtion prints string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0;
+	while (s[b] != '\0')
+	b++;
+
+	b = strlen(s);
+	for (a = b - 1; a >= 0; a--)
+	{
+	_putchar(s[a]);
+	}
+	_putchar('\n');
+}
