c730e51a051fa7b7e1b2310c1673445df4e7e3aa
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-13 04:10:54 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - this function points to an int and update its value to 98
+ * @n: variable
+ */
+void reset_to_98(int *n)
+{
+	int n = 402;
+
+	_putchar("%d", n);
+	*n = 98;
+	_putchar("%d\n", *n);
+	_putchar('\n');
+}
