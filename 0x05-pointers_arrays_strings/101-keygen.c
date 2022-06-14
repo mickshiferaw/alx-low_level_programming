c9ef87d308a2f697e5682c6428a1217fb5d8d047
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-13 17:11:12 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Random number generator
+ * Return: returns zero
+ */
+int main (void)
+{
+	char d [100];
+	int a, b, c;
+
+	b = 0;
+	c = 0;
+	srand(time(NULL));
+	while (b < 2645)
+	{
+	a = rand() % 122;
+	if (a > 32)
+	{
+		d[c++] = a;
+		b += a;
+	}
+	}
+	d[c++] = (2772 - n);
+	d[c] = '\0';
+	printf("%s", d);
+	return (0);
+}
