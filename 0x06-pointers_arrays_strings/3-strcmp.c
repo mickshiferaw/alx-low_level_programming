16b97f80672ff68d4a0f7e18593b070be992f049
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-16 13:42:49 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - This function copies a string
+ * @s1: parameter
+ * @s2: parameter
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = _strcmp(s1, s2);
+	return (0);
+}
