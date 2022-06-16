b82f7edb688f6eb570b8b869f110134c4e66a85c
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-16 09:55:28 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+include "main.h"
+/**
+ * _strncat - Function that concatenates two strings
+ * @dest: parameter
+ * @src:parameter
+ * @n: parameter
+ * Return: Returns string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	_strncat(dest, src, n);
+	return (dest);
+}
