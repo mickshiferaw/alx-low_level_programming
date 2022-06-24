fb7b6c86cd72325cd421828ef5254e3aa9bb3671
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-24 08:32:35 -0700

    lib final

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,17 +1,13 @@
-#include <stdio.h>
-/**
- * main - Main code prints _putchar
- * Return: Returns zero
- */
-int main(void)
-{
-	char a[9] = "_putchar";
-	int m;
+#include <unistd.h>
 
-	for (m = 0; m < 8; m++)
-	{
-		putchar(a[m]);
-	}
-	putchar ('\n');
-	return (0);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
