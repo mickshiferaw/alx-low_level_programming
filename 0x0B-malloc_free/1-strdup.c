185a27a50b949dfca6e275dd14e096d20f436d7d
Michael Bekele <shiferawmichael3@gmail.com>
2022-06-28 13:49:18 -0700

    task 1 fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,12 +10,12 @@ char *_strdup(char *str)
 	char *s;
 
 	if (str == NULL)
-		return (NULL);
+		return ('\0');
 	while (str[a])
 		a++;
 	s = (char *)malloc(a * sizeof(char) + 1);
 	if (s == NULL)
-		return (NULL);
+		return ('\0');
 	while (b < a)
 	{
 		s[b] = str[b];
