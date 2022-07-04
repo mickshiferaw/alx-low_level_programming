52ca40a13408e5fc70011b89ef629acfb0bfb5ef
Michael Bekele <shiferawmichael3@gmail.com>
2022-07-04 15:00:09 -0700

    fix1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,14 +5,14 @@
  * @name: name
  * @age: age
  * @owner: owner
+ * @d: struct
  * Return: none
- * Description: Longer description
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == '\0')
-    return;
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	if (d == '\0')
+	return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
