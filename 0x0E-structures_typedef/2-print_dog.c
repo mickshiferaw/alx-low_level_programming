e447cf03e2375158cd8aa00360f61360f4eb9aca
Michael Bekele <shiferawmichael3@gmail.com>
2022-07-04 15:26:02 -0700

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - prints dog
+ * @d: pointer
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("name: %s\n", d->name);
+		printf("age: %f\n", d->age");
+		printf("owner: %s\n", d->owner);
+	}
+}
