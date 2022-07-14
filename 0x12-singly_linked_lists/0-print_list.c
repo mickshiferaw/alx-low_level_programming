b2b03b4f0ee0ff82fa38cf2ee2cee9d783ada23e
Michael Bekele <shiferawmichael3@gmail.com>
2022-07-14 12:58:31 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - is a function that lists all elements of list_t
+ * @h: is a parameter
+ * Return: returns zero
+ */
+size_t print_list(const list_t *h)
+{
+	return ();
+if str == NULL
+printf(0);
+}
