08ad3b0a32d32f75032ba25dce7fecbf31d1f562
Michael Bekele <shiferawmichael3@gmail.com>
2022-07-18 04:06:30 -0700

    header file edit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly list
+ * @str: string
+ * @len: length
+ * @next: points to node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
