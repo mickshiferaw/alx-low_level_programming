2912c90094bb5bb09eb9e5835ac1bf9ca89ec74a
Michael Bekele <shiferawmichael3@gmail.com>
2022-07-18 03:58:45 -0700

    task 0 fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,22 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * print_list - is a function that lists all elements of list_t
  * @h: is a parameter
  * Return: returns zero
  */
 size_t print_list(const list_t *h)
-{
-	int a;
+{ 
 
-	struct node *a = malloc(sizeof(size_t));
-	return (*a);
-	if str == NULL
+	for (size_t m = 0;, h != NULL)
 	{
-		printf(0);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		m++;
+		h = h->next;
 	}
+
+	return (m);
 }
