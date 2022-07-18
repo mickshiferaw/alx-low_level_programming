beb8073c1a53e05ed8ebc1b74c361ba1299d246c
Michael Bekele <shiferawmichael3@gmail.com>
2022-07-18 04:52:59 -0700

    advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_text - prints a str
+ */
+void print_text(void) __attribute__((constructor));
+
+void print_text(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back! \n");
+}
