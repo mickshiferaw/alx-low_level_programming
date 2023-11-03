f94eb21a1993928ead5603df67b5d4bd8ab903e3
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-11-03 05:23:03 -0700

    fix01

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *ht;
-	unsigned long int x;
+    unsigned long int x;
 
     ht = malloc(sizeof(hash_table_t));
     if (ht == NULL)
